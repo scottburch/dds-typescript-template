824d40a7a0d675f8acfbcc5b7a36c924d324505a
Scott Burch <scott@bulldoginfo.com>
2023-02-08 14:02:16 -0700

    update pistol version

--- src/components/CountBtn.tsx
+++ src/components/CountBtn.tsx
@@ -1,9 +1,10 @@
 import {putPistolValue, usePistolValue} from "@scottburch/pistol";
+import {firstValueFrom} from "rxjs";
 
 export const CountBtn: React.FC = () => {
     const count = usePistolValue<number>('demo-app.counter');
 
-    const doCount = () => putPistolValue('demo-app.counter', (count || 0) + 1).subscribe();
+    const doCount = () => firstValueFrom(putPistolValue('demo-app.counter', (count || 0) + 1));
 
     return (
         <button onClick={doCount}>Press me</button>
