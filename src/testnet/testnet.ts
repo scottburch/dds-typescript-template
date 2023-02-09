6b1714ef048b33555ad3631e31134488375bdfd6
Scott Burch <scott@bulldoginfo.com>
2023-02-09 16:12:33 -0700

    cleaned up testnet starter

--- src/testnet/testnet.ts
+++ src/testnet/testnet.ts
@@ -1,11 +1,10 @@
 import {dialPeer, floodRouter, newMemoryStore, startPistol} from "@scottburch/pistol";
-import {merge, tap, bufferCount, switchMap} from 'rxjs'
+import {tap, combineLatest, switchMap} from 'rxjs'
 
-merge(
-    startPistol({extensions: [floodRouter()], port: 11110, store: newMemoryStore()}),
-    startPistol({extensions: [floodRouter()], port: 11111, store: newMemoryStore()}),
-).pipe(
-    bufferCount(2),
+combineLatest([
+    startPistol({name: 'testnet-0', extensions: [floodRouter()], port: 11110, store: newMemoryStore()}),
+    startPistol({name: 'testnet-1', extensions: [floodRouter()], port: 11111, store: newMemoryStore()})
+]).pipe(
     switchMap(pistols => dialPeer(pistols[0], 'ws://localhost:11111')),
     tap(() => console.log('testnet running'))
 ).subscribe()
