12b53f62243660d78c867889fc72b58d4cb9d00d
Scott Burch <scott@bulldoginfo.com>
2023-02-25 20:48:35 -0700

    updated for new pistol which removes extensions property

--- src/testnet/testnet.ts
+++ src/testnet/testnet.ts
@@ -2,8 +2,12 @@ import {dialPeer, floodRouter, newMemoryStore, startPistol} from "@scottburch/pi
 import {tap, combineLatest, switchMap} from 'rxjs'
 
 combineLatest([
-    startPistol({name: 'testnet-0', extensions: [floodRouter()], port: 11110, store: newMemoryStore()}),
-    startPistol({name: 'testnet-1', extensions: [floodRouter()], port: 11111, store: newMemoryStore()})
+    startPistol({name: 'testnet-0', port: 11110, store: newMemoryStore()}).pipe(
+        switchMap(floodRouter)
+    ),
+    startPistol({name: 'testnet-1', port: 11111, store: newMemoryStore()}).pipe(
+        switchMap(floodRouter)
+    )
 ]).pipe(
     switchMap(pistols => dialPeer(pistols[0], 'ws://localhost:11111')),
     tap(() => console.log('testnet running'))
